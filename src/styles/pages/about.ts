a04d1861a7d88a20377275c257056e7a89b513c6
marceloaraujo28 <marcelo_araujo28@hotmail.com>
2024-01-31 20:17:15 -0300

    feat: Ajusted About Style

---
+++ src/styles/pages/about.ts
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+export const AboutContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+`;
+
+export const MainContainer = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  flex: 1;
+`;
+
+export const TextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 408px;
+  align-items: flex-start;
+  margin-top: 34px;
+  margin-bottom: 72px;
+`;
+
+export const Title = styled.h1`
+  font-weight: 600;
+  font-size: 18px;
+`;
+
+export const Subtitle = styled.h2`
+  font-weight: 500;
+  font-size: 16px;
+`;
+
+export const Text = styled.p`
+  font-size: 14px;
+`;
