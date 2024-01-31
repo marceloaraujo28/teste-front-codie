a41746f119c3c5f3225161fad1b62addfc9bb358
marceloaraujo28 <marcelo_araujo28@hotmail.com>
2024-01-31 17:12:35 -0300

    feat: Created types Button component

---
+++ src/components/Button/types.ts
@@ -0,0 +1,8 @@
+import { ButtonHTMLAttributes, ReactNode } from "react";
+
+export type ButtonProps = {
+  hasBackground?: boolean;
+  link: string;
+  textColor: string;
+  children: ReactNode;
+} & ButtonHTMLAttributes<HTMLButtonElement>;
