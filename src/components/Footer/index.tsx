9268cfbd304d8a487d38ce59912a948fb93e3aea
marceloaraujo28 <marcelo_araujo28@hotmail.com>
2024-01-31 17:15:48 -0300

    feat: Created Footer Component

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+import { FooterContainer, FooterText } from "./styles";
+
+export default function Footer() {
+  return (
+    <FooterContainer>
+      <FooterText>
+        Todas as marcas e ilustrações utilizadas são de seus respectivos donos.
+      </FooterText>
+    </FooterContainer>
+  );
+}
---
+++ src/components/Footer/styles.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const FooterContainer = styled.div`
+  height: 72px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #1d1d1d;
+`;
+
+export const FooterText = styled.p`
+  color: #fff;
+  font-size: 14px;
+`;
