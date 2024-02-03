aebb6d4bb1b220bc4474e1ad755779cccc61e767
marceloaraujo28 <marcelo_araujo28@hotmail.com>
2024-02-03 14:01:42 -0300

    feat: created and finished validation form

--- src/pages/scheduling/index.tsx
+++ src/pages/scheduling/index.tsx
@@ -3,6 +3,8 @@ import { ButtonAdd, ButtonLink } from "@/src/components/Button";
 import Info from "@/src/components/Info";
 import Input from "@/src/components/Input";
 import Select from "@/src/components/Select";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { formValidation } from "@/src/helpers/formValidation";
 import {
   FormContainer,
   HeaderDescription,
@@ -19,27 +21,74 @@ import {
   ValueTotal,
 } from "@/src/styles/pages/scheduling";
 
+import { SubmitHandler, useForm } from "react-hook-form";
+
+interface IForm {
+  name: string;
+  surname: string;
+  region: string;
+  city: string;
+  poke1: string;
+  date: string;
+  time: string;
+}
+
 export default function Scheduling() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<IForm>({
+    resolver: yupResolver(formValidation),
+  });
+
+  const onSubmit: SubmitHandler<IForm> = (data) => console.log("TESTE");
+
   return (
     <SchedulingContainer>
       <Breadcrumb page="Agendar Consulta" link="/scheduling" />
       <Title>Preencha o formulário abaixo para agendar sua consulta</Title>
-      <FormContainer>
+      <FormContainer onSubmit={handleSubmit(onSubmit)}>
         <TopForm>
           <InputZone>
-            <Input labelName="Nome" type="text" placeholder="Digite seu nome" />
+            <Input
+              labelName="Nome"
+              type="text"
+              placeholder="Digite seu nome"
+              errorMessage={errors.name?.message}
+              {...register("name")}
+            />
             <Input
               labelName="Sobrenome"
               type="text"
               placeholder="Digite seu sobrenome"
+              errorMessage={errors.surname?.message}
+              {...register("surname")}
             />
           </InputZone>
           <InputZone>
-            <Select labelName="Região">
-              <option value="">Marcelo</option>
+            <Select
+              labelName="Região"
+              {...register("region")}
+              defaultValue={""}
+              errorMessage={errors.region?.message}
+            >
+              <option value="" disabled hidden>
+                Escolha uma região
+              </option>
+              <option value="Marcelo">Marcelo</option>
             </Select>
-            <Select labelName="Cidade">
-              <option value="">Marcelo</option>
+            <Select
+              labelName="Cidade"
+              {...register("city")}
+              defaultValue={""}
+              errorMessage={errors.city?.message}
+            >
+              <option value="" disabled hidden>
+                Selecione sua cidade
+              </option>
+              <option value="Marcelo">Marcelo</option>
             </Select>
           </InputZone>
         </TopForm>
@@ -50,19 +99,41 @@ export default function Scheduling() {
               Atendemos até 06 pokémons por vez
             </HeaderDescription>
           </HeaderMid>
-          <Select labelName="Pokémon" labelSide="left">
-            <option value="">Marcelo</option>
-          </Select>
-          <Select labelName="Pokémon" labelSide="left">
-            <option value="">Marcelo</option>
+          <Select
+            labelName="Pokémon"
+            labelSide="left"
+            defaultValue={""}
+            errorMessage={errors.poke1?.message}
+            {...register("poke1")}
+          >
+            <option value="" disabled hidden>
+              Selecione seu pokémon
+            </option>
+            <option value="Marcelo">Marcelo</option>
           </Select>
           <ButtonAdd>Adicionar novo pokémon ao time...</ButtonAdd>
           <InputZone>
-            <Select labelName="Data para Atendimento">
-              <option value="">Marcelo</option>
+            <Select
+              labelName="Data para Atendimento"
+              {...register("date")}
+              errorMessage={errors.date?.message}
+              defaultValue={""}
+            >
+              <option value="" disabled hidden>
+                Selecione uma data
+              </option>
+              <option value="Marcelo">Marcelo</option>
             </Select>
-            <Select labelName="Horário de Atendimento">
-              <option value="">Marcelo</option>
+            <Select
+              labelName="Horário de Atendimento"
+              {...register("time")}
+              errorMessage={errors.time?.message}
+              defaultValue={""}
+            >
+              <option value="" disabled hidden>
+                Selecione um horário
+              </option>
+              <option value="Marcelo">Marcelo</option>
             </Select>
           </InputZone>
         </MidForm>
@@ -77,7 +148,7 @@ export default function Scheduling() {
           </ImportantText>
           <Submit>
             <ValueTotal>Valor Total: R$ 72,10</ValueTotal>
-            <ButtonLink link="/" hasBackground textColor="#fff">
+            <ButtonLink type="submit" hasBackground textColor="#fff">
               Concluir Agendamento
             </ButtonLink>
           </Submit>
