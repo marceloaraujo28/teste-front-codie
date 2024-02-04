5c6b38180f9e35769a729933747337c9912058ae
marceloaraujo28 <marcelo_araujo28@hotmail.com>
2024-02-04 18:16:54 -0300

    feat: configured usequery in the app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,25 @@
 import type { AppProps } from "next/app";
 import { GlobalStyle } from "@/src/styles/global";
 import { Container } from "@/src/styles/pages/app";
+import { QueryClient, QueryClientProvider, Hydrate } from "react-query";
 
 import Footer from "@/src/components/Footer";
 import Header from "@/src/components/Header";
+import React from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const queryClient = React.useRef(new QueryClient());
+
   return (
-    <Container>
-      <GlobalStyle />
-      <Header />
-      <Component {...pageProps} />
-      <Footer />
-    </Container>
+    <QueryClientProvider client={queryClient.current}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Container>
+          <GlobalStyle />
+          <Header />
+          <Component {...pageProps} />
+          <Footer />
+        </Container>
+      </Hydrate>
+    </QueryClientProvider>
   );
 }
