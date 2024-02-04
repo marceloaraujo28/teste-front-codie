c6f633f3b37d5a63737b1ced93748a5a1d328a1c
marceloaraujo28 <marcelo_araujo28@hotmail.com>
2024-02-04 18:18:24 -0300

    feat: added priority background image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,6 +10,7 @@ export default function Home() {
         src={backgroundHome}
         className="background-image"
         alt="Imagem de fundo"
+        priority
       />
       <TextOverlay>
         <p>
