e5d61792594bf0fdaff7dd0069b47bbea0bb61c4
marceloaraujo28 <marcelo_araujo28@hotmail.com>
2024-02-04 18:15:38 -0300

    feat: created pokemonservice

---
+++ src/api/service/PokemonService.ts
@@ -0,0 +1,63 @@
+import { PokemonClient } from "../client/PokemonClient";
+import {
+  IGetAllRegions,
+  IGetInfoArea,
+  IGetInfoCity,
+  IGetInfoRegion,
+} from "../types";
+import { UseQuery } from "../useQuery";
+
+export class PokemonService {
+  private pokemonClient = new PokemonClient();
+
+  getInfoCity(city: string) {
+    return UseQuery<IGetInfoCity>({
+      queryKey: [`getInfoCityKey${city}`],
+      queryFn: async () => {
+        return await this.pokemonClient.getInfoCity(city);
+      },
+      options: {
+        enabled: !!city,
+        cacheTime: 600000,
+      },
+    });
+  }
+
+  getInfoRegion(region: string) {
+    return UseQuery<IGetInfoRegion>({
+      queryKey: [`getInfoRegion${region}`],
+      queryFn: async () => {
+        return await this.pokemonClient.getInfoRegion(region);
+      },
+      options: {
+        enabled: !!region,
+        cacheTime: 600000,
+      },
+    });
+  }
+
+  getAllRegions() {
+    return UseQuery<IGetAllRegions>({
+      queryKey: "getAllRegionsKey",
+      queryFn: async () => {
+        return await this.pokemonClient.getAllRegions();
+      },
+      options: {
+        cacheTime: 600000,
+      },
+    });
+  }
+
+  getInfoArea(url: string) {
+    return UseQuery<IGetInfoArea>({
+      queryKey: `getInfoAreaKey${url}`,
+      queryFn: async () => {
+        return await this.pokemonClient.getInfoArea(url);
+      },
+      options: {
+        enabled: !!url,
+        cacheTime: 600000,
+      },
+    });
+  }
+}
