ae395f4942bf02af69536088e8c7137ad128750d
marceloaraujo28 <marcelo_araujo28@hotmail.com>
2024-02-06 03:02:20 -0300

    feat: removed unnecessary styles

--- src/styles/pages/scheduling.ts
+++ src/styles/pages/scheduling.ts
@@ -22,43 +22,6 @@ export const FormContainer = styled.form`
   align-items: center;
 `;
 
-export const TopForm = styled.div`
-  display: flex;
-  gap: 32px;
-  flex-direction: column;
-`;
-
-export const InputZone = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  gap: 8px;
-`;
-
-export const MidForm = styled.div`
-  width: 548px;
-  margin-top: 109px;
-  display: flex;
-  flex-direction: column;
-  gap: 32px;
-`;
-
-export const HeaderMid = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 8px;
-`;
-
-export const HeaderName = styled.h3`
-  font-weight: 700;
-  font-size: 12px;
-`;
-
-export const HeaderDescription = styled.p`
-  font-size: 12px;
-  font-weight: 500px;
-`;
-
 export const InfosContent = styled.div`
   margin-top: 32px;
   width: 548px;
