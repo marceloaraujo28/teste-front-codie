cb95b82382d03a9e9d5368df37ab7ce8bcbeac22
marceloaraujo28 <marcelo_araujo28@hotmail.com>
2024-02-06 03:01:25 -0300

    feat:

---
+++ src/components/MidForm/styles.ts
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+export const InputZone = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 8px;
+`;
+
+export const MidFormContainer = styled.div`
+  width: 548px;
+  margin-top: 109px;
+  display: flex;
+  flex-direction: column;
+  gap: 32px;
+`;
+
+export const HeaderMid = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+`;
+
+export const HeaderName = styled.h3`
+  font-weight: 700;
+  font-size: 12px;
+`;
+
+export const HeaderDescription = styled.p`
+  font-size: 12px;
+  font-weight: 500px;
+`;
